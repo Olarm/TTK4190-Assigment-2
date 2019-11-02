9d9e92fcbe34566048f62c5dabd7868555f2f896
Ola Rasmus Moen <ola.moen@wellstarter.com>
2019-11-02 19:49:56 +0100

    modell for kalman filter

--- airplaneModel.m
+++ airplaneModel.m
@@ -1,4 +1,4 @@
-function model = airplaneModel(q, r)
+function model = airplaneModel(Q, R)
     % returns a structure that implements a discrete time CV model with
     % continuous time accelleration covariance q and positional
     % measurement with noise with covariance r, both in two dimensions.
@@ -8,17 +8,16 @@ function model = airplaneModel(q, r)
                         -10.6 0 -2.87 0.46;
                         6.87 0 -0.04 -0.32] * x(1:4);
                     
-    model.Ak = @(x, Ts) [-0.322 0.052 0.028 -1.12;  % F
+    model.Ak = @() [-0.322 0.052 0.028 -1.12;  % F
                         0 0 1 -0.001;
                         -10.6 0 -2.87 0.46;
                         6.87 0 -0.04 -0.32];
                     
-    model.Bk = @(u, Ts) [0.002; 0; -0.65; -0.02] * u;   
-    model.Q = @(x, Ts) q * eye(4);
-    model.R = @(x,Ts) r * eye(2);
+    model.Bk = @() [0.002; 0; -0.65; -0.02];   
+    model.Q = @() Q;
+    model.R = @() R;
     
-    model.ck = @(x) [[0 0; 0 0] eye(2,2)] * x(1:4);          % h
-    model.Ck = @(x) [[0 0; 0 0] eye(2,2)];          % H
+    model.Ck = @() [[0 0; 0 0] eye(2,2)];          % H
 
     
     model.Ek = @(x) eye(4);
